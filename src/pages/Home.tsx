7e65cfe543c4b7e4dfbd774c8465ab09763ce5dd
YedixonRDev <yedixonjrf@gmail.com>
2024-12-18 17:25:37 -0500

    ajustes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,14 @@ import GlobalBrands from "@/components/Home-GlobalBrands"
 import Testimonial from "@/components/Home-Testimonial"
 import HomeWhyChosee from "@/components/Home-WhyChosee"
 import Footer from "@/components/Footer"
-
+import HomeTrustpilot from "@/components/Home-Trustpilot"
 const Home = () => {
    return(
       <div className="min-w-full bg-gray-100 flex flex-col justify-center pt-24 items-center min-h-screen">
          <Welcome/>
          <GlobalBrands/>
          <Testimonial/>
+         <HomeTrustpilot/>
          <HomeWhyChosee/>
          <AboutMe/>
          <Footer/>
