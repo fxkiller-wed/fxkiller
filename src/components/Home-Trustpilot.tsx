acc81efeab9adbfab6885c0eecec45365a95c090
YedixonRDev <yedixonjrf@gmail.com>
2024-12-26 15:39:25 -0500

    main

--- src/components/Home-Trustpilot.tsx
+++ src/components/Home-Trustpilot.tsx
@@ -21,14 +21,13 @@ const HomeTrustpilot = () => {
           Calificación de nuestros clientes
         </p>
       </div>
-      {/* <a
-        href="#"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="contact-btn-form-home text-blue1 px-6 py-2 font-urbanist font-semibold text-sm md:text-base rounded-xl"
-      >
-        Ver más opiniones
-      </a> */}
+      <div className="flex justify-center items-center">
+        <div className="trustpilot-widget" data-locale="es-ES" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="676c5f6781011505a3a0e065" data-style-height="52px" data-style-width="100%">
+          <a href="https://es.trustpilot.com/review/fxkillers.mx" target="_blank" rel="noopener noreferrer" className="contact-btn-form-home text-blue1 px-6 py-3 font-urbanist font-semibold text-sm md:text-base rounded-xl">
+            Ver más opiniones
+          </a>
+        </div>
+      </div>
     </motion.div>
   );
 };
