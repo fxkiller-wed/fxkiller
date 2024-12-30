bdb03f966780bb15bb836a7b8e625796e9aa78a6
YedixonRDev <yedixonjrf@gmail.com>
2024-12-29 22:40:59 -0500

    Elimine el boton de trustpilot

--- src/components/Home-Trustpilot.tsx
+++ src/components/Home-Trustpilot.tsx
@@ -13,18 +13,18 @@ const HomeTrustpilot = () => {
       <img
         src={TrustpilotLogo}
         alt="Trustpilot Logo"
-        className="w-36 md:w-44 object-contain"
+        className="w-36 md:w-60 object-contain"
       />
       <div className="font-urbanist text-lg md:text-2xl text-beige2 text-center">
         <span className="text-beige2 font-semibold">4.87 ★★★★★</span>
       </div>
-      <div className="flex justify-center items-center mt-3 lg:mt-0">
+      {/* <div className="flex justify-center items-center mt-3 lg:mt-0">
         <div className="trustpilot-widget" data-locale="es-ES" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="676c5f6781011505a3a0e065" data-style-height="52px" data-style-width="100%">
           <a href="https://es.trustpilot.com/review/fxkillers.mx" target="_blank" rel="noopener noreferrer" className="contact-btn-form-home text-blue1 px-6 py-3 font-urbanist font-semibold text-sm md:text-base rounded-xl">
             Ver más opiniones
           </a>
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
